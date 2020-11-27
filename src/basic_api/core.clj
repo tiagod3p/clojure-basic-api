53922bf23d2967f425d1c8bb509b990876af9eef
Tiago Vidal <tiagod3p@gmail.com>
2020-11-26 21:07:43 -0300

    feat: test ring http request with run-jetty function

--- src/basic_api/core.clj
+++ src/basic_api/core.clj
@@ -1,7 +1,15 @@
 (ns basic-api.core
+  (:use ring.adapter.jetty)
   (:gen-class))
+
+(defn handler [request]
+  {:status 200
+   :headers {"Content-Type" "text/plain"}
+   :body "Hello World"})
 
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
   (println "Hello, World!"))
+
+(run-jetty handler {:port 3000})
