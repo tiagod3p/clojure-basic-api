9ccf5fafaaa6cf31bb49ac8026b5aa17684187fb
Tiago Vidal <tiagod3p@gmail.com>
2020-12-18 22:15:34 -0300

    refactor: add ring json and refactor api to use content type as application/json

--- project.clj
+++ project.clj
@@ -4,8 +4,8 @@
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
   :dependencies [[org.clojure/clojure "1.10.0"]
-                 [org.clojure/data.json "1.0.0"]
                  [ring "1.8.2"]
+                 [ring/ring-json "0.5.0"]
                  [compojure "1.6.2"]]
   :plugins [[lein-cljfmt "0.7.0"]]
   :main ^:skip-aot basic-api.core
--- src/basic_api/core.clj
+++ src/basic_api/core.clj
@@ -1,12 +1,23 @@
 (ns basic-api.core
   (:require [ring.adapter.jetty :refer [run-jetty]]
+            [ring.middleware.json :refer [wrap-json-response]]
+            [ring.util.response :refer [response]]
             [compojure.core :refer [GET defroutes]]
             [compojure.route :as route])
   (:gen-class))
 
+(defn handler [request]
+  (response {:body {:statusCode "200" :message "Home Page"}}))
+
+(defn route_not_found [request]
+  (response {:body {:statusCode "404" :message "Page not found"}}))
+
+(def app
+  (wrap-json-response handler))
+
 (defroutes index
-  (GET "/" [] "TEST")
-  (route/not-found "404 not found"))
+  (GET "/test" [] app)
+  (route/not-found (wrap-json-response route_not_found)))
 
 (defn -main
   []
